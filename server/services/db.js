90d24139d87f3f789cd77ed1a497d649e1d0dbc0
Noon <noonrimdusit16@gmail.com>
2022-10-02 16:03:57 +0700

    first commit

---
+++ server/services/db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "", // << Change this password (✿◠‿◠) 
+    database: 'mydb'  // << Change this database name (っ＾▿＾)💨
+});
+
+conn.connect();
+
+module.exports = conn;
